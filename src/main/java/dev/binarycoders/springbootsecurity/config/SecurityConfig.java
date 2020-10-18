0526decaf185ada269a17781a245317ae606ca21
fjavierm <fco.javier.marquez@gmail.com>
2020-10-18 12:03:10 +0100

    Add basic authentication. Keep actuator endpoint available

--- src/main/java/dev/binarycoders/springbootsecurity/config/SecurityConfig.java
+++ src/main/java/dev/binarycoders/springbootsecurity/config/SecurityConfig.java
@@ -3,14 +3,27 @@ package dev.binarycoders.springbootsecurity.config;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.requestMatcher(EndpointRequest.toAnyEndpoint())
-            .authorizeRequests((requests) -> requests.anyRequest().permitAll());
+        http
+            .authorizeRequests()
+            // Allow all actuator endpoints
+            .requestMatchers(EndpointRequest.toAnyEndpoint())
+            .permitAll()
+            
+            // Anything else needs to be authenticated
+            .antMatchers("/**")
+            .authenticated()
+
+            // Mechanism is basic authentication
+            .and()
+            .httpBasic();
     }
 }
